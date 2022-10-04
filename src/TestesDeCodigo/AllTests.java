228bbb307953a044df86337df7ebf10077614354
Jenifer19IFC <jenifergoedert10@gmail.com>
2022-10-04 19:44:57 -0300

    testesDeCodigo

---
+++ src/TestesDeCodigo/AllTests.java
@@ -0,0 +1,12 @@
+package TestesDeCodigo;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith (Suite.class )
+@SuiteClasses ({ JUnitTeste.class, JUnitTeste1.class })
+
+public class AllTests {
+	
+}
---
+++ src/TestesDeCodigo/PessoaMain.java
@@ -0,0 +1,13 @@
+package TestesDeCodigo;
+
+public class PessoaMain {
+
+	public static void main(String[] args) {
+	
+		Pessoa p = new Pessoa("Jênifer","Goedert");
+		System.out.println(p.nomeCompleto());
+		
+
+	}
+
+}
